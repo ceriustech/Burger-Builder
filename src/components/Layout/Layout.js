33ee490a23dd30c481236935d6891b335ff33e14
Bill <ceriustech@gmail.com>
2020-03-31 22:14:23 -0500

    Adding CRA to project.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './Layout.module.css'; 
+
+
+const layout = (props) => {
+    return (
+        <>
+            <div>Toolbar, SideDrawer, Backdrop</div>
+            <main className={classes.content}>
+                {props.children}
+            </main>
+        </>
+    )
+};
+
+export default layout; 
