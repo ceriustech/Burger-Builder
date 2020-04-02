d75b7a091775bcaab5af1ab927b56ec87a0a7264
Bill <ceriustech@gmail.com>
2020-04-01 22:06:46 -0500

    Added function to burger and burgerIngredients components.

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const burgerIngredients = (props) => {
+    let ingredient = null; 
+
+    switch (props.type) {
+        case('bread-bottom'):
+            
+    }
+};
+
+export default burgerIngredients; 
