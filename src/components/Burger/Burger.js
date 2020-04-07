fa86738be6d67c89a8cc83fff2e50b0b538c606e
Bill <ceriustech@gmail.com>
2020-04-06 22:11:55 -0500

    Added component for controlling add/remove elements from the ui.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import classes from './Burger.module.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredients key={igKey + i} type={igKey} />
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients</p>; 
+    }
+
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top" />
