e5a96d870c9c4b192cd60abc8afaab45e7da98fb
Bill <ceriustech@gmail.com>
2020-04-12 22:27:19 -0500

    Added a toolbar component to the application.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Layout.module.css'; 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 
 const layout = (props) => {
     return (
         <>
-            <div>Toolbar, SideDrawer, Backdrop</div>
+            <Toolbar />
             <main className={classes.content}>
                 {props.children}
             </main>
