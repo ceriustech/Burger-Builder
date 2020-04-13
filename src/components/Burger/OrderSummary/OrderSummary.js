9fdaf58ca545a081c633a4af37f32e533718a578
Bill <ceriustech@gmail.com>
2020-04-12 22:04:28 -0500

    Added a button component that will either cancel or continue orders.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,6 @@
-import React from 'react'; 
+import React from 'react';
+
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -13,7 +15,10 @@ const orderSummary = (props) => {
           <ul>
               {ingredientSummary}
           </ul>
+          <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
           <p>Checkout</p>
+          <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
+          <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
         </>
     );
 
