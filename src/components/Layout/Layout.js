2d7a86e6b79638a1779ea2693156c321bee981b1
Bill <ceriustech@gmail.com>
2020-04-22 08:02:40 -0500

    Adjusted logo styling for the toolbar navigation.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classes from './Layout.module.css'; 
 import Toolbar from '../Navigation/NavigationItems/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
 const layout = (props) => {
     return (
         <>
             <Toolbar />
+            <SideDrawer />
             <main className={classes.content}>
                 {props.children}
             </main>
