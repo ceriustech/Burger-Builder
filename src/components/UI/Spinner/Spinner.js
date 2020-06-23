841ce5f14a00f1fec61ecb91bf339dada15e69e7
Bill <ceriustech@gmail.com>
2020-06-22 20:34:13 -0500

    Added axios, set up a firebase backend, and added a spinner component for rendering a loading animation after submitting an order.

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Spinner.module.css';
+
+const Spinner = () => (
+    <div class={classes.Loader}>Loading...</div>
+);
+
+export default Spinner; 
